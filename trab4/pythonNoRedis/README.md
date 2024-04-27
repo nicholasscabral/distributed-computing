d6694923b68d9b826a897ad837efab7b72dc80aa
Thais Rozas <thaisrozas@pop-os.localdomain>
2024-04-27 19:08:04 -0300

    feat: python and ruby with no redis (cache service)

---
+++ trab4/pythonNoRedis/README.md
@@ -0,0 +1,13 @@
+# Link Extractor: Step 4
+
+A web application to extract links and anchor texts from a given web page and analyze link statistics.
+
+## Try it out
+
+```
+$ docker-compose up --build
+```
+
+Open http://localhost/?url=http%3A%2F%2Fodu.edu%2F in a web browser.
+
+Press `Ctrl + C` to terminate the service.
--- trab4/rubyNoRedis/api/linkextractor.rb
+++ trab4/rubyNoRedis/api/linkextractor.rb
@@ -6,14 +6,12 @@ require "open-uri"
 require "uri"
 require "nokogiri"
 require "json"
-require "redis"
+
 
 set :protection, :except=>:path_traversal
 
-redis = Redis.new(url: ENV["REDIS_URL"] || "redis://localhost:6379")
 
-Dir.mkdir("logs") unless Dir.exist?("logs")
-cache_log = File.new("logs/extraction.log", "a")
+
 
 get "/" do
   "Usage: http://<hostname>[:<prt>]/api/<url>"
@@ -21,15 +19,11 @@ end
 
 get "/api/*" do
   url = [params['splat'].first, request.query_string].reject(&:empty?).join("?")
-  cache_status = "HIT"
-  jsonlinks = redis.get(url)
-  if jsonlinks.nil?
-    cache_status = "MISS"
+ 
     jsonlinks = JSON.pretty_generate(extract_links(url))
-    redis.set(url, jsonlinks)
-  end
+  
 
-  cache_log.puts "#{Time.now.to_i}\t#{cache_status}\t#{url}"
+ 
 
   status 200
   headers "content-type" => "application/json"
