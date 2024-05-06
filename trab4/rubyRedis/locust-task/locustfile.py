48ed5a842835b9358e375d926658e142ef0fc743
Thais Rozas Teixeira <thaisrozasteixeira@pop-os.localdomain>
2024-05-06 13:14:29 -0300

    fix: update graphs with new urls per user

--- trab4/rubyRedis/locust-task/locustfile.py
+++ trab4/rubyRedis/locust-task/locustfile.py
@@ -6,16 +6,16 @@ class QuickstartUser(HttpUser):
     @task
     def extract_link(self):
         urls = [
-            "https://www.reddit.com/r/brdev/",
-            "https://news.ycombinator.com/",
-            "https://github.com/spring-projects/spring-boot",
-            "https://github.com/topics/aws",
-            "https://github.com/covid19india/covid19india-react",
-            "https://github.com/carlosruan12307?tab=repositories",
-            "https://github.com/etcd-io/etcd",
-            "https://github.com/twbs/bootstrap",
-            "https://github.com/macrozheng/mall",
-            "https://github.com/facebook/react-native"
+            "https://github.com/",
+            "https://stackoverflow.com/",
+            "https://techcrunch.com/",
+            "https://arstechnica.com/",
+            "https://www.theverge.com/",
+            "https://www.wired.com/",
+            "https://www.producthunt.com/",
+            "https://slashdot.org/",
+            "https://www.cnet.com/",
+            "https://medium.com/"
         ]
         for url in urls:
             self.client.get(f"/?url={url}")
